b07ab95d26d1e172bebcdeff7712de1497da10aa
ichtrojan <ordrizzy@gmail.com>
2019-11-28 17:04:06 +0100

    Routes ✈

---
+++ routes/web.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/ichtrojan/go-todo/controllers"
+)
+
+func Init() *mux.Router {
+	route := mux.NewRouter()
+
+	route.HandleFunc("/", controllers.Show)
+	route.HandleFunc("/add", controllers.Add).Methods("POST")
+
+	return route
+}
