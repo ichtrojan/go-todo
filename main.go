81566be304b2418798efbe9e6db761c254d25818
ichtrojan <ordrizzy@gmail.com>
2020-02-02 00:56:05 +0100

    Log to file

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"github.com/ichtrojan/go-todo/routes"
+	"github.com/ichtrojan/thoth"
 	"log"
 	"net/http"
 )
 
 func main() {
+	logger, _ := thoth.Init("log")
+
+	//if err := godotenv.Load(); err != nil {
+	//	logger.Log(errors.New("no .env file found"))
+	//	log.Fatal("No .env file found")
+	//}
+
 	err := http.ListenAndServe(":90", routes.Init())
 
 	if err != nil {
+		logger.Log(err)
 		log.Fatal(err)
 	}
 }
