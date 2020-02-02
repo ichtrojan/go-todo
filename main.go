47d129f63c08efd15b073f3e5a902444cb38faad
ichtrojan <ordrizzy@gmail.com>
2020-02-02 01:04:31 +0100

    Check IF .env exists | Added custom ports

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"errors"
 	"github.com/ichtrojan/go-todo/routes"
 	"github.com/ichtrojan/thoth"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	logger, _ := thoth.Init("log")
 
-	//if err := godotenv.Load(); err != nil {
-	//	logger.Log(errors.New("no .env file found"))
-	//	log.Fatal("No .env file found")
-	//}
+	if err := godotenv.Load(); err != nil {
+		logger.Log(errors.New("no .env file found"))
+		log.Fatal("No .env file found")
+	}
 
-	err := http.ListenAndServe(":90", routes.Init())
+	port, exist := os.LookupEnv("PORT")
+
+	if !exist {
+		logger.Log(errors.New("PORT not set in .env"))
+		log.Fatal("PORT not set in .env")
+	}
+
+	err := http.ListenAndServe(":"+port, routes.Init())
 
 	if err != nil {
 		logger.Log(err)
